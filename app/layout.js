aad0418a9cac97351aa89071767f2dad5ff85aa6
ashabb <ashishgole99955@gmail.com>
2024-10-01 09:49:40 +0530

    u

--- app/layout.js
+++ app/layout.js
@@ -35,7 +35,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-
+      <link rel="icon" type="image/svg+xml" href="/logo.ico" />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
